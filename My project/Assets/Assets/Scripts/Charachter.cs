f74ddbac321dcf294c7300423ed695f94242c84a
Hiki-01 <hikigaya0.1v@gmail.com>
2023-07-23 16:51:21 +0300

    hiki

---
+++ My project/Assets/Assets/Scripts/Charachter.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charachter : MonoBehaviour
+{ public int PlayerChar=0;
+    public GameObject Char0;
+    public GameObject Char1;
+    public GameObject Char2;
+    public GameObject Char3;
+    public GameObject Char4;
+    public GameObject hide;
+
+
+
+    public void Button0()
+  {
+     Char0.GetComponent<SpriteRenderer>().enabled = true;
+     Char1.GetComponent<SpriteRenderer>().enabled = false;
+     Char2.GetComponent<SpriteRenderer>().enabled = false;
+     Char3.GetComponent<SpriteRenderer>().enabled = false;
+     Char4.GetComponent<SpriteRenderer>().enabled = false;
+     hide.SetActive(false);
+
+     Char0.GetComponent<PlayerMovement>().enabled = true;
+     Char0.GetComponent<cameraController>().enabled = false;
+}
+        public void Button1()
+        {
+            Char0.GetComponent<SpriteRenderer>().enabled = false;
+            Char1.GetComponent<SpriteRenderer>().enabled = true;
+            Char2.GetComponent<SpriteRenderer>().enabled = false;
+            Char3.GetComponent<SpriteRenderer>().enabled = false;
+            Char4.GetComponent<SpriteRenderer>().enabled = false;
+            hide.SetActive(false);
+
+            Char1.GetComponent<PlayerMovement>().enabled = true;
+            Char1.GetComponent<cameraController>().enabled = false;
+        }
+        public void Button2()
+        {
+            Char0.GetComponent<SpriteRenderer>().enabled = false;
+            Char1.GetComponent<SpriteRenderer>().enabled = false;
+            Char2.GetComponent<SpriteRenderer>().enabled = true;
+            Char3.GetComponent<SpriteRenderer>().enabled = false;
+            Char4.GetComponent<SpriteRenderer>().enabled = false;
+            hide.SetActive(false);
+
+            Char2.GetComponent<PlayerMovement>().enabled = true;
+            Char2.GetComponent<cameraController>().enabled = false;
+        }
+        public void Button3()
+        {
+            Char0.GetComponent<SpriteRenderer>().enabled = false;
+            Char1.GetComponent<SpriteRenderer>().enabled = false;
+            Char2.GetComponent<SpriteRenderer>().enabled = false;
+            Char3.GetComponent<SpriteRenderer>().enabled = true;
+            Char4.GetComponent<SpriteRenderer>().enabled = false;
+            hide.SetActive(false);
+
+            Char3.GetComponent<PlayerMovement>().enabled = true;
+            Char3.GetComponent<cameraController>().enabled = false;
+        }
+        public void Button4()
+        {
+            Char0.GetComponent<SpriteRenderer>().enabled = false;
+            Char1.GetComponent<SpriteRenderer>().enabled = false;
+            Char2.GetComponent<SpriteRenderer>().enabled = false;
+            Char3.GetComponent<SpriteRenderer>().enabled = false;
+            Char4.GetComponent<SpriteRenderer>().enabled = true;
+            hide.SetActive(false);
+ 
+            Char4.GetComponent<PlayerMovement>().enabled = true;
+            Char4.GetComponent<cameraController>().enabled = false;
+        }
+    
+}
---
+++ My project/Assets/Assets/Scripts/Choose.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choose : MonoBehaviour
+{
+    public GameObject ChoosenOne;
+    private int n = 0;
+    private void OnMouseEnter()
+    {
+        n =+ n;
+        
+            if (n == 1)
+            {
+                ChoosenOne.SetActive(true);
+            }
+            else if (n > 1 || n < 0)
+            {
+            ChoosenOne.SetActive(false);
+            n = 0;
+            }
+    }
+   
+}
