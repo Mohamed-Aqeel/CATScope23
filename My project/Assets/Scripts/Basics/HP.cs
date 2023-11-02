375167c2f4ed208e6ef881b54155d786d000343b
Hiki-01 <hikigaya0.1v@gmail.com>
2023-11-02 16:37:00 +0300

    player

---
+++ My project/Assets/Scripts/Basics/HP.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class HP : MonoBehaviour
+{
+    public int maxLives = 3; // Maximum lives of the player
+    private int currentLives; // Current lives of the player
+
+    public Image[] livesImages; // An array of Image components representing player lives
+
+    private void Start()
+    {
+        currentLives = maxLives;
+        UpdateLivesUI();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap") || collision.gameObject.CompareTag("Boss")) // Check if player collides with trap or boss
+        {
+            TakeDamage();
+            UpdateLivesUI();
+        }
+    }
+    public void TakeDamage()
+    {
+        currentLives--;
+
+        if (currentLives <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // Perform actions when the player runs out of lives, e.g., show game over screen, reset level, etc.
+        Debug.Log("Player has died!");
+        // Add your game over logic here
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void UpdateLivesUI()
+    {
+        // Iterate through the livesImages array and disable images for lost lives
+        for (int i = 0; i < livesImages.Length; i++)
+        {
+            if (i < currentLives)
+            {
+                livesImages[i].enabled = true; // Enable image for remaining lives
+            }
+            else
+            {
+                livesImages[i].enabled = false; // Disable image for lost lives
+            }
+        }
+    }
+}
---
+++ My project/Assets/Scripts/Level1/RockShooter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class RockShooter : MonoBehaviour
+{
+    public GameObject rockPrefab; // Prefab of the rock object to be shot
+    public Transform shootPoint; // Point where rocks will be spawned
+    public float shootInterval = 1f; // Time interval between rock shots
+    private bool playerInRange; // Flag to check if player is in the shooter's range
+    public float shootSpeed = 1f; // Speed  for rock shots
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            InvokeRepeating("ShootRock", 0f, shootInterval);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            CancelInvoke("ShootRock");
+        }
+    }
+
+    private void ShootRock()
+    {
+        if (playerInRange)
+        {
+            GameObject rock = Instantiate(rockPrefab, shootPoint.position, Quaternion.identity);
+            // Add force to shoot the rock (customize the force as needed)
+            Instantiate(rockPrefab,shootPoint);
+        }
+    }
+}
